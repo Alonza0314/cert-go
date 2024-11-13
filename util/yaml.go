412b517079f0a5e9b80a980cc522dd2ae6363df0
Alonza0314 <dufengplus@gmail.com>
2024-11-13 18:57:51 +0000

    feat: add read yaml file to struct function and test

--- util/yaml.go
+++ util/yaml.go
@@ -24,3 +24,19 @@ func ReadYamlFile(filePath string) (map[string]interface{}, error) {
 
 	return data, nil
 }
+
+func ReadYamlFileToStruct(filePath string, v interface{}) error {
+	yamlFile, err := os.ReadFile(filePath)
+	if err != nil {
+		logger.Error("ReadYamlFile: " + err.Error())
+		return err
+	}
+
+	err = yaml.Unmarshal(yamlFile, v)
+	if err != nil {
+		logger.Error("ReadYamlFileToStruct: " + err.Error())
+		return err
+	}
+
+	return nil
+}
--- util/yaml_test.go
+++ util/yaml_test.go
@@ -3,6 +3,8 @@ package util
 import (
 	"reflect"
 	"testing"
+
+	"github.com/Alonza0314/cert-go/model"
 )
 
 var testCaseMap = []struct {
@@ -14,24 +16,113 @@ var testCaseMap = []struct {
 		expect: map[string]interface{}{
 			"ca": map[string]interface{}{
 				"root": map[string]interface{}{
-					"cert":        "./default_ca/root/root_cert.pem",
-					"private_key": "./default_ca/root/root_key.pem",
-					"csr":         "./default_ca/root/root_csr.pem",
+					"type":           "root",
+					"cert":           "./default_ca/root/root_cert.pem",
+					"private_key":    "./default_ca/root/root_key.pem",
+					"csr":            "./default_ca/root/root_csr.pem",
+					"is_ca":          true,
+					"organization":   "default_ca",
+					"common_name":    "default_ca",
+					"validity_years": 10,
+					"validity_month": 0,
+					"validity_day":   0,
 				},
 				"intermediate": map[string]interface{}{
-					"cert":        "./default_ca/intermediate/intermediate_cert.pem",
-					"private_key": "./default_ca/intermediate/intermediate_key.pem",
-					"csr":         "./default_ca/intermediate/intermediate_csr.pem",
+					"type":           "intermediate",
+					"cert":           "./default_ca/intermediate/intermediate_cert.pem",
+					"private_key":    "./default_ca/intermediate/intermediate_key.pem",
+					"csr":            "./default_ca/intermediate/intermediate_csr.pem",
+					"is_ca":          true,
+					"organization":   "default_ca",
+					"common_name":    "default_ca",
+					"validity_years": 10,
+					"validity_month": 0,
+					"validity_day":   0,
 				},
 				"server": map[string]interface{}{
-					"cert":        "./default_ca/server/server_cert.pem",
-					"private_key": "./default_ca/server/server_key.pem",
-					"csr":         "./default_ca/server/server_csr.pem",
+					"type":           "server",
+					"cert":           "./default_ca/server/server_cert.pem",
+					"private_key":    "./default_ca/server/server_key.pem",
+					"csr":            "./default_ca/server/server_csr.pem",
+					"is_ca":          false,
+					"organization":   "default_ca",
+					"common_name":    "default_ca",
+					"validity_years": 10,
+					"validity_month": 0,
+					"validity_day":   0,
 				},
 				"client": map[string]interface{}{
-					"cert":        "./default_ca/client/client_cert.pem",
-					"private_key": "./default_ca/client/client_key.pem",
-					"csr":         "./default_ca/client/client_csr.pem",
+					"type":           "client",
+					"cert":           "./default_ca/client/client_cert.pem",
+					"private_key":    "./default_ca/client/client_key.pem",
+					"csr":            "./default_ca/client/client_csr.pem",
+					"is_ca":          false,
+					"organization":   "default_ca",
+					"common_name":    "default_ca",
+					"validity_years": 10,
+					"validity_month": 0,
+					"validity_day":   0,
+				},
+			},
+		},
+	},
+}
+
+var testCaseStruct = []struct {
+	filePath string
+	expect   model.CAConfig
+}{
+	{
+		filePath: "../cfg.yml",
+		expect: model.CAConfig{
+			CA: model.CertificateAuthority{
+				Root: model.Certificate{
+					Type:          "root",
+					CertFilePath:  "./default_ca/root/root_cert.pem",
+					KeyFilePath:   "./default_ca/root/root_key.pem",
+					CsrFilePath:   "./default_ca/root/root_csr.pem",
+					IsCA:          true,
+					Organization:  "default_ca",
+					CommonName:    "default_ca",
+					ValidityYears: 10,
+					ValidityMonth: 0,
+					ValidityDay:   0,
+				},
+				Intermediate: model.Certificate{
+					Type:          "intermediate",
+					CertFilePath:  "./default_ca/intermediate/intermediate_cert.pem",
+					KeyFilePath:   "./default_ca/intermediate/intermediate_key.pem",
+					CsrFilePath:   "./default_ca/intermediate/intermediate_csr.pem",
+					IsCA:          true,
+					Organization:  "default_ca",
+					CommonName:    "default_ca",
+					ValidityYears: 10,
+					ValidityMonth: 0,
+					ValidityDay:   0,
+				},
+				Server: model.Certificate{
+					Type:          "server",
+					CertFilePath:  "./default_ca/server/server_cert.pem",
+					KeyFilePath:   "./default_ca/server/server_key.pem",
+					CsrFilePath:   "./default_ca/server/server_csr.pem",
+					IsCA:          false,
+					Organization:  "default_ca",
+					CommonName:    "default_ca",
+					ValidityYears: 10,
+					ValidityMonth: 0,
+					ValidityDay:   0,
+				},
+				Client: model.Certificate{
+					Type:          "client",
+					CertFilePath:  "./default_ca/client/client_cert.pem",
+					KeyFilePath:   "./default_ca/client/client_key.pem",
+					CsrFilePath:   "./default_ca/client/client_csr.pem",
+					IsCA:          false,
+					Organization:  "default_ca",
+					CommonName:    "default_ca",
+					ValidityYears: 10,
+					ValidityMonth: 0,
+					ValidityDay:   0,
 				},
 			},
 		},
@@ -40,12 +131,29 @@ var testCaseMap = []struct {
 
 func TestReadYamlFile(t *testing.T) {
 	for _, testCase := range testCaseMap {
-		data, err := ReadYamlFile(testCase.filePath)
-		if err != nil {
-			t.Errorf("TestReadYamlFile: %v", err)
-		}
-		if !reflect.DeepEqual(data, testCase.expect) {
-			t.Errorf("TestReadYamlFile: actual %v != expect %v", data, testCase.expect)
-		}
+		t.Run(testCase.filePath, func(t *testing.T) {
+			data, err := ReadYamlFile(testCase.filePath)
+			if err != nil {
+				t.Errorf("TestReadYamlFile: %v", err)
+			}
+			if !reflect.DeepEqual(data, testCase.expect) {
+				t.Errorf("TestReadYamlFile: actual %v != expect %v", data, testCase.expect)
+			}
+		})
+	}
+}
+
+func TestReadYamlFileToStruct(t *testing.T) {
+	for _, testCase := range testCaseStruct {
+		t.Run(testCase.filePath, func(t *testing.T) {
+			var actual model.CAConfig
+			err := ReadYamlFileToStruct(testCase.filePath, &actual)
+			if err != nil {
+				t.Errorf("TestReadYamlFileToStruct: %v", err)
+			}
+			if !reflect.DeepEqual(actual, testCase.expect) {
+				t.Errorf("TestReadYamlFileToStruct: actual %v != expect %v", actual, testCase.expect)
+			}
+		})
 	}
 }
