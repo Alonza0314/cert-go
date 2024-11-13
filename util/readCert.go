5ee492df3e7ec10e6b72b1bae9e05f84e41a8da6
Alonza0314 <dufengplus@gmail.com>
2024-11-13 19:12:50 +0000

    feat: add read cert function

---
+++ util/readCert.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+	"os"
+
+	"github.com/Alonza0314/cert-go/logger"
+)
+
+func ReadCertificate(certPath string) ([]byte, error) {
+	// 讀取憑證檔案
+	certBytes, err := os.ReadFile(certPath)
+	if err != nil {
+		logger.Error("ReadCertificate: " + err.Error())
+		return nil, err
+	}
+
+	block, _ := pem.Decode(certBytes)
+	if block == nil {
+		logger.Error("ReadCertificate: failed to decode PEM block")
+		return nil, errors.New("failed to decode PEM block")
+	}
+
+
+	_, err = x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		logger.Error("ReadCertificate: " + err.Error())
+		return nil, err
+	}
+
+	return block.Bytes, nil
+}
