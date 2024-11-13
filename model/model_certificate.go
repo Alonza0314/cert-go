8b39f91416bdd55bf7b0829f10257915e32b7912
Alonza0314 <dufengplus@gmail.com>
2024-11-13 18:58:51 +0000

    feat: add new entry

--- model/model_certificate.go
+++ model/model_certificate.go
@@ -5,20 +5,31 @@ import (
 	"crypto/x509"
 )
 
+type CAConfig struct {
+	CA CertificateAuthority `yaml:"ca"`
+}
+
+type CertificateAuthority struct {
+	Root         Certificate `yaml:"root"`
+	Intermediate Certificate `yaml:"intermediate"`
+	Server       Certificate `yaml:"server"`
+	Client       Certificate `yaml:"client"`
+}
+
 type Certificate struct {
-	Type         string
-	CertFilePath string
-	KeyFilePath  string
-	CsrFilePath  string
+	Type         string `yaml:"type"`
+	CertFilePath string `yaml:"cert"`
+	KeyFilePath  string `yaml:"private_key"`
+	CsrFilePath  string `yaml:"csr"`
 
 	ParentCert    *x509.Certificate
 	ParentKey     *ecdsa.PrivateKey
-	IsCA          bool
-	Organization  string
-	CommonName    string
-	ValidityYears int
-	ValidityMonth int
-	ValidityDay   int
+	IsCA          bool   `yaml:"is_ca"`
+	Organization  string `yaml:"organization"`
+	CommonName    string `yaml:"common_name"`
+	ValidityYears int    `yaml:"validity_years"`
+	ValidityMonth int    `yaml:"validity_month"`
+	ValidityDay   int    `yaml:"validity_day"`
 	KeyUsage      x509.KeyUsage
 	ExtKeyUsage   []x509.ExtKeyUsage
 }
