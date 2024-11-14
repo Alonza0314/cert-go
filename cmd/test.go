a0d06a0dada95cc96011f9c0bcdc796136e5cfb1
Alonza0314 <dufengplus@gmail.com>
2024-11-14 15:03:42 +0000

    test: add test cmd

--- cmd/test.go
+++ cmd/test.go
@@ -11,7 +11,9 @@ import (
 
 // testCmd represents the test command
 var testCmd = &cobra.Command{
-	Use: "test",
+	Use:   "test",
+	Short: "test command has no actual function",
+	Long:  `test command has no actual function, it's just for testing that the tool is working properly`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("test success")
 	},
