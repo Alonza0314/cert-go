409fbf5a108936613a74bb62f5eeffbb4d4faf2a
Alonza0314 <dufengplus@gmail.com>
2025-01-03 11:57:24 +0000

    feat: add skip create if key exist

--- privateKey_test.go
+++ privateKey_test.go
@@ -8,13 +8,20 @@ import (
 )
 
 var testCasePrivateKey = []struct {
-	name   string
+	name    string
 	keyPath string
+	exist   bool
 	expect  *ecdsa.PrivateKey
 }{
 	{
-		name:   "test",
+		name:    "test without exist",
 		keyPath: "./default_ca/test.key.pem",
+		exist:   false,
+	},
+	{
+		name:    "test with exist",
+		keyPath: "./default_ca/test.key.pem",
+		exist:   true,
 	},
 }
 
@@ -22,21 +29,24 @@ func TestCreatePrivateKey(t *testing.T) {
 	for _, testCase := range testCasePrivateKey {
 		t.Run(testCase.name, func(t *testing.T) {
 			privateKey, err := CreatePrivateKey(testCase.keyPath)
-			if err != nil {
-				t.Fatalf("TestCreatePrivateKey: %v", err)
-			}
-			if privateKey == nil {
-				t.Fatalf("TestCreatePrivateKey: private key is nil")
-			}
-			readPrivateKey, err := util.ReadPrivateKey(testCase.keyPath)
-			if err != nil {
-				t.Fatalf("TestCreatePrivateKey: %v", err)
-			}
-			if readPrivateKey == nil {
-				t.Fatalf("TestCreatePrivateKey: read private key is nil")
-			}
-			if privateKey.D.Cmp(readPrivateKey.D) != 0 {
-				t.Fatalf("TestCreatePrivateKey: private key is not equal")
+			if testCase.exist {
+				if err == nil || err.Error() != "private key already exists" {
+					t.Fatalf("TestCreatePrivateKey: private key should exist")
+				}
+			} else {
+				if privateKey == nil {
+					t.Fatalf("TestCreatePrivateKey: private key is nil")
+				}
+				readPrivateKey, err := util.ReadPrivateKey(testCase.keyPath)
+				if err != nil {
+					t.Fatalf("TestCreatePrivateKey: %v", err)
+				}
+				if readPrivateKey == nil {
+					t.Fatalf("TestCreatePrivateKey: read private key is nil")
+				}
+				if privateKey.D.Cmp(readPrivateKey.D) != 0 {
+					t.Fatalf("TestCreatePrivateKey: private key is not equal")
+				}
 			}
 		})
 	}
