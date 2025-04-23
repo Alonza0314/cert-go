e5b098ec131e66f644645f6cd74a6aa041d2fc01
Alonza0314 <a0984078318@gmail.com>
2025-04-24 01:11:15 +0800

    feat: add key type flag in cmd

--- cmd/privateKey.go
+++ cmd/privateKey.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	certgo "github.com/Alonza0314/cert-go"
+	"github.com/Alonza0314/cert-go/constants"
 	logger "github.com/Alonza0314/logger-go"
 	"github.com/spf13/cobra"
 )
@@ -18,8 +19,13 @@ var privateKeyCmd = &cobra.Command{
 func init() {
 	privateKeyCmd.Flags().StringP("out", "o", "", "specify the output path of the private key")
 	privateKeyCmd.Flags().BoolP("force", "f", false, "overwrite the private key if it already exists")
+	privateKeyCmd.Flags().StringP("key", "k", "", "specify the type of the private key, <ecdsa> or <rsa>")
 
 	if err := privateKeyCmd.MarkFlagRequired("out"); err != nil {
+		logger.Error("cert-go", err.Error())
+	}
+
+	if err := privateKeyCmd.MarkFlagRequired("key"); err != nil {
 		logger.Error("cert-go", err.Error())
 	}
 
@@ -38,8 +44,21 @@ func createPrivateKey(cmd *cobra.Command, args []string) {
 		return
 	}
 
+	keyType, err := cmd.Flags().GetString("key")
+	if err != nil {
+		logger.Error("cert-go", err.Error())
+		return
+	}
+
+	var privateKeyType constants.PrivateKeyType
+	if keyType == "rsa" {
+		privateKeyType = constants.PRIVATE_KEY_TYPE_RSA
+	} else {
+		privateKeyType = constants.PRIVATE_KEY_TYPE_ECDSA
+	}
+
 	logger.Info("cert-go", "start to create private key")
-	if _, err := certgo.CreatePrivateKey(outputPath, force); err != nil {
+	if _, err := certgo.CreatePrivateKey(outputPath, privateKeyType, force); err != nil {
 		if strings.Contains(err.Error(), "already exists") {
 			logger.Error("cert-go", "failed to create private key")
 			return
